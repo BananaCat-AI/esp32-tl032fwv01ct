2925cca007db01ea77b75bbdd2d7938cb1c590f8
jpmuedano <113537046+jpmuedano@users.noreply.github.com>
2025-01-19 15:31:10 -0500

    Updated pin-map.py script to show microcontroller pin names with board alias

--- utils/pin-map.py
+++ utils/pin-map.py
@@ -2,12 +2,20 @@
 import microcontroller
 import board
 
+"""
+The pin names available in the CircuitPython `board` module are NOT the same as the names of the pins
+on the microcontroller itself! The `board` pin names are `aliases` to the microcontroller pin names.
+If you look at the datasheet for your microcontroller, you'll likely find a pinout with a series of pin names!
+In this script we are accessing the `microcontroller.pin` module to get the microcontroller pin name.
+The mapping results in: `board.<pin alias> (<microcontroller pin name>)` where the <pin alias> is the same as 
+the one printed on the board.
+"""
 board_pins = []
-for pin in dir(board):
-    if isinstance(getattr(board, pin), microcontroller.Pin):
+for pin in dir(microcontroller.pin):
+    if isinstance(getattr(microcontroller.pin, pin), microcontroller.Pin):
         pins = []
         for alias in dir(board):
-            if getattr(board, alias) is getattr(board, pin):
+            if getattr(board, alias) is getattr(microcontroller.pin, pin):
                 pins.append(f"board.{alias}")
         # Add the original GPIO name, in parentheses
         if pins:
